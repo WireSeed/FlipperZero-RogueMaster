02ff23aaf3cbad3f9f0f838c3a3eaed8a5da2718
RogueMaster <anifan115@gmail.com>
2023-05-20 06:15:46 -0400

    FMT Changes

--- applications/main/subghz/.subghz_dangerous_freq.c
+++ applications/main/subghz/.subghz_dangerous_freq.c
@@ -13,10 +13,8 @@ void subghz_dangerous_freq() {
     FlipperFormat* fff_data_file = flipper_format_file_alloc(storage);
 
     if(flipper_format_file_open_existing(fff_data_file, "/ext/subghz/assets/extend_range.txt")) {
-        flipper_format_read_bool(
-            fff_data_file, "use_ext_range_at_own_risk", &is_extended_i, 1);
-        flipper_format_read_bool(
-            fff_data_file, "ignore_default_tx_region", &is_bypassed_i, 1);
+        flipper_format_read_bool(fff_data_file, "use_ext_range_at_own_risk", &is_extended_i, 1);
+        flipper_format_read_bool(fff_data_file, "ignore_default_tx_region", &is_bypassed_i, 1);
     }
 
     // Not set up to store and use a variable on tests yet (function below re-sets the settings on the file that it just read)
